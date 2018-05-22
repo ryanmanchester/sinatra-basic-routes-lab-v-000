f0540a819a4d0dda59ae13c7ecd7d567d2ce5585
Ryan Manchester <ryanm17@gmail.com>
2018-05-22 02:22:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,6 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get('/name') {"My name is Ryan"}
-
-
+  get('/hometown') {"My hometown is Amarillo, Texas"}
+  get('/favorite-song') {"My favorite song is 'Subterraneans'"}
 end
